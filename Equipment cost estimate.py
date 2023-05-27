76c6541293d2ed325363e715effd995547a4f946
Pratik <89375766+Carbonn14@users.noreply.github.com>
2023-05-27 15:25:43 -0600

    Delete Equipment cost estimate.py

--- Equipment cost estimate.py
+++
@@ -1,6 +0,0 @@
-#Cost estimate of equipment
-Ci=int(input('Enter cost of your equipment: '))
-Si=int(input(f'Enter size of your equipment: '))
-Sf=int(input(f'Enter size of required equipments in same units: '))
-Cf=Ci*(Sf/Si)**0.6
-print(f'Cost of required equipment: {Cf}')
